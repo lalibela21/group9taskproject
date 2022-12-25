25fbb098b88ec1f234286af47cad75ae6159e3e2
lalibela21 <104536319+lalibela21@users.noreply.github.com>
2022-12-24 21:42:44 -0500

    front end

--- script.js
+++ script.js
@@ -49,15 +49,15 @@ let createTasks = () => {
     data.map((x, y) => {
         return (tasks.innerHTML += ` 
             <div id=${y}>
-                <span class="fw-bold">${x.text}</span>
-                <span class="small text-secondary">${x.date}</span>
+                <span class="fw-bold text-uppercase">${x.text}</span>
+                <span class="small ">${x.date}</span>
                     <p>${x.description}</p>
     
                 <span class="options">
                     <i onClick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-                    <i onClick="deleteTask(this);createTasks()" class="far fa-trash-alt"></i>
+                    <i onClick="deleteTask(this);createTasks()" class="far fa-trash-alt  "></i>
                 </span>
-            </div>
+            </div> 
         `);
     });
     
